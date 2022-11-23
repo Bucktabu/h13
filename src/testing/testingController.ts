1db6b05549c2365cdb558b76f4b7714b3422dd95
Stanislav <stanislaworonin@gmail.com>
2022-11-23 19:23:09 +0300

    change run script

--- src/testing/testingController.ts
+++ src/testing/testingController.ts
@@ -1,6 +1,5 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import mongoose from 'mongoose';
-import { UsersRepository } from '../users/infrastructure/users.repository';
 
 @Controller('testing')
 export class TestingController {
