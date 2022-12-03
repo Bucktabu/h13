2a029fe49faffb0190da18f50963d50fe5480145
Stanislav <stanislaworonin@gmail.com>
2022-12-03 08:14:28 +0300

    fix

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
   MONGO_URI:
     process.env.mongoURI ||
-    'mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority',
+    'mongodb://0.0.0.0:27017/blogPlatform?maxPoolSize=20&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || '123',
   BASIC_USER: 'admin',
   BASIC_PASS: 'qwerty',
